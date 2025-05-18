b60ec2dc27607c0866dbf2a4a49ae095a7bf92ca
arafat22184 <123alarafat@gmail.com>
2025-05-19 04:12:00 +0600

    added patch method to users DB

--- index.js
+++ index.js
@@ -83,6 +83,16 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users", async (req, res) => {
+      const { email, lastSignInTime } = req.body;
+      const filter = { email: email };
+      const updatedDoc = {
+        $set: { lastSignInTime: lastSignInTime },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
